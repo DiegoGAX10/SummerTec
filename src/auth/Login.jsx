fde787015ff482c4ba9efdf9d0dd6e07ad0aa5f5
¨Diego <dieegoortiz24@gmail.com>
2025-03-13 14:45:36 -0600

    fix: login and SignUp

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
 import axios from "axios";
-import { Alert } from "react-native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { CommonActions } from "@react-navigation/native";
+import { useNavigate } from "react-router-dom";
 
-const Login = ({ navigation }) => {
+const Login = () => {
     const [controlNumber, setControlNumber] = useState('');
     const [password, setPassword] = useState('');
+    const navigate = useNavigate();
 
     const generateEmail = (controlNumber) => {
         return `L${controlNumber}@zacatepec.tecnm.mx`;
@@ -23,84 +22,57 @@ const Login = ({ navigation }) => {
 
             if (response.status === 200) {
                 const { localId } = response.data;
-
-                // Guarda el UID en AsyncStorage
-                await AsyncStorage.setItem('userUID', localId);
-                Alert.alert('Inicio de sesión exitoso');
-
-                // Navega al Main y resetea el historial
-                navigation.dispatch(
-                    CommonActions.reset({
-                        index: 0,
-                        routes: [{ name: 'Main' }],
-                    })
-                );
+                localStorage.setItem('userUID', localId);
+                window.alert('Inicio de sesión exitoso');
+                navigate('/main');
             } else {
-                Alert.alert('Error de autenticación', 'No se pudo iniciar sesión. Intenta de nuevo.');
+                window.alert('Error de autenticación. Intenta de nuevo.');
             }
         } catch (error) {
             console.error('Error en el inicio de sesión:', error);
             if (error.response && error.response.status === 401) {
-                Alert.alert('Error', 'Número de control o contraseña incorrectos');
+                window.alert('Número de control o contraseña incorrectos');
             } else {
-                Alert.alert('Error', 'Ocurrió un error en el servidor');
+                window.alert('Ocurrió un error en el servidor');
             }
         }
     };
 
     return (
-        <section className="bg-gray-50 dark:bg-gray-900">
+        <section className="bg-gray-50">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                 <img className="w-35 mb-8" src="/summer_tec.svg" alt="logo" />
 
-                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                            Inicia sesión
-                        </h1>
+                        <h1 className="text-xl font-bold text-gray-900 md:text-2xl">Inicia sesión</h1>
                         <form className="space-y-4 md:space-y-6" onSubmit={handleLogin}>
                             <div>
-                                <label htmlFor="controlNumber" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                <label htmlFor="controlNumber" className="block mb-2 text-sm font-medium text-gray-900">
                                     Número de control
                                 </label>
-                                <input type="text" name="controlNumber" id="controlNumber"
-                                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                <input type="text" id="controlNumber"
+                                       className="bg-gray-50 border text-gray-900 rounded-lg focus:ring-primary-600 block w-full p-2.5"
                                        value={controlNumber} onChange={(e) => setControlNumber(e.target.value)} required
                                 />
                             </div>
 
                             <div>
-                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">
                                     Contraseña
                                 </label>
-                                <input type="password" name="password" id="password" placeholder="••••••••"
-                                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                <input type="password" id="password" placeholder="••••••••"
+                                       className="bg-gray-50 border text-gray-900 rounded-lg focus:ring-primary-600 block w-full p-2.5"
                                        value={password} onChange={(e) => setPassword(e.target.value)} required
                                 />
                             </div>
 
-                            <div className="flex items-start">
-                                <div className="flex items-center h-5">
-                                    <input id="terms" aria-describedby="terms" type="checkbox"
-                                           className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                                           required
-                                    />
-                                </div>
-                                <div className="ml-3 text-sm">
-                                    <label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">
-                                        Acepto los <a
-                                        className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                                        href="#">Términos y Condiciones</a>
-                                    </label>
-                                </div>
-                            </div>
                             <button type="submit"
-                                    className="w-full text-white bg-[var(--primary-color)] cursor-pointer hover:bg-blue-950 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                                    className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5">
                                 Iniciar sesión
                             </button>
-                            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                ¿Aún no tienes cuenta? <a href="/register"
-                                                          className="font-medium text-primary-600 hover:underline dark:text-primary-500">
+                            <p className="text-sm text-gray-500">
+                                ¿Aún no tienes cuenta? <a href="/register" className="text-blue-600 hover:underline">
                                 Regístrate aquí
                             </a>
                             </p>
--- src/auth/Register.jsx
+++ src/auth/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Alert } from 'react-native';
+import { Alert } from 'react';
 
 const Register = ({ navigation }) => {
     const [numero_control, setNumeroControl] = useState('');
