8274c066f17bf31d5bfde42054f2044e61613415
¨Diego <dieegoortiz24@gmail.com>
2025-03-25 15:58:43 -0600

    Modifying App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Landing from './components/Landing.jsx';
-import Login from "./auth/Login.jsx";
-import Register from "./auth/Register.jsx";
+import Login from './auth/Login.jsx';
+import Register from './auth/Register.jsx';
 import './App.css';
-import Inicio from "./students/inicio/Inicio.jsx";
+import Inicio from './students/inicio/Inicio.jsx';
+import { SobreNosotros, PrivacyPolicy, OurMission, OurTeam } from './components/AboutPages.jsx';
+
 function App() {
     return (
         <Router>
@@ -14,6 +16,11 @@ function App() {
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/home" element={<Inicio />} />
+                <Route path="/sobre-nosotros" element={<SobreNosotros />} />
+                <Route path="/politica-de-privacidad" element={<PrivacyPolicy />} />
+                <Route path="/nuestra-mision" element={<OurMission />} />
+                <Route path="/nuestro-equipo" element={<OurTeam />} />
+                {/* Add other routes here */}
             </Routes>
         </Router>
     );
