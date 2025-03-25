fdc5f164bdebeb4feb3ae4e528cf0d2c8ade7c55
¨Diego <dieegoortiz24@gmail.com>
2025-03-25 14:10:35 -0600

    fix: Login url

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
         event.preventDefault();
         try {
             const email = generateEmail(controlNumber);
-            const response = await axios.post('https://summer-tec-backend.onrender.com/auth/signup', {
+            const response = await axios.post('https://summer-tec-backend.onrender.com/auth/login', {
                 email: email,
                 password: password,
             });
