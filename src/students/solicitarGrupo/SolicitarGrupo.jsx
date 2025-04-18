bc072f1553f243f2e1502c3c38fabb48a211759b
aldoSN003 <j.aldosn05@gmail.com>
2025-04-18 00:15:48 -0600

    fix: Form for create materia propuesta is now dynamic, it fetches materias based on the carrera selected, further improvements with profesor data should be handled

--- src/students/solicitarGrupo/SolicitarGrupo.jsx
+++ src/students/solicitarGrupo/SolicitarGrupo.jsx
@@ -8,7 +8,7 @@ import axios from "axios";
 import Swal from "sweetalert2";
 import {SlChemistry} from "react-icons/sl";
 import Dropdown from "../../components/global/Dropdown.jsx";
-import { useNavigate } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 
 
 export default function SolicitarGrupo() {
@@ -16,51 +16,7 @@ export default function SolicitarGrupo() {
     const userType = localStorage.getItem('userType');
     const userEmail = localStorage.getItem('userEmail');
 
-
-    const navigate = useNavigate();
-
-
-//const [claveCarrera, setClaveCarrera] = useState(false);
-    const [materiasClave, setMateriasClave] = useState([]);
-
-
-    async function getMateriasByClave() {
-        try {
-            const response = await axios.get(`http://127.0.0.1:5000/materias/materias_by_clave_carrera/ISIC-2010-224`,
-                {
-                    headers: {
-                        Authorization: `Bearer ${token}`
-                    }
-                });
-
-            setMateriasClave(response.data);
-
-
-        } catch (error) {
-            console.error('Error al obtener materias:', error);
-            await Swal.fire({
-                title: "Error",
-                text: "No se pudieron obtener las materias.",
-                icon: "error",
-            });
-        }
-    }
-
-
-    useEffect(() => {
-        getMateriasByClave(); // Call the function to fetch materias
-    }, []); // Empty dependency array means this runs once when the component mounts
-
-    useEffect(() => {
-        if (materiasClave.length > 0) {
-            console.log("✅ materiasClave actualizadas:", JSON.stringify(materiasClave, null, 2));
-        } else {
-            console.log("⚠️ materiasClave está vacía");
-        }
-    }, [materiasClave]);
-
-
-    const [matSelec, setMatSelec] = useState(materias[0]); // Initialize with the first materia
+    const [matSelec, setMatSelec] = useState('');
     const [carrera, setCarrera] = useState();
     const [materiaId, setMateriaId] = useState([]);
 
@@ -69,6 +25,51 @@ export default function SolicitarGrupo() {
     const [trabajaLocal, setTrabajaLocal] = useState("Sí");
     const [profesor, setProfesor] = useState("");
     const [correoProfesor, setCorreoProfesor] = useState("");
+
+
+    //Informacion de la materia
+    const [creditos, setCreditos] = useState("");
+    const [hrsSemana, setHrsSemana] = useState("");
+    const [claveMateria, setClaveMateria] = useState([]);
+
+
+    const navigate = useNavigate();
+
+
+    const [materiasClave, setMateriasClave] = useState([]);
+
+
+    async function getMateriasByClave() {
+        if (!carrera || !carrera.clave) return; // prevent empty requests
+
+        try {
+            const response = await axios.get(`http://127.0.0.1:5000/materias/materias_by_clave_carrera/${carrera.clave}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+
+            setMateriasClave(response.data);
+            await console.log(JSON.stringify(response, null, 2))
+
+        } catch (error) {
+            console.error('Error al obtener materias:', error);
+            setMateriasClave([]);
+
+            if (error.response && error.response.status === 404) {
+                await Swal.fire({
+                    title: "Sin materias registradas",
+                    html: `No se encontraron materias para <strong>${carrera.label}</strong>`,
+                    icon: "info"
+                });
+            } else {
+                await Swal.fire({
+                    title: "Error", text: "No se pudieron obtener las materias.", icon: "error"
+                });
+            }
+        }
+    }
+
 
     async function handleSubmit() {
         const data = {
@@ -82,156 +83,181 @@ export default function SolicitarGrupo() {
 
         try {
             const response = await fetch("http://127.0.0.1:5000/materias_propuestas/create_materia_propuesta", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": `Bearer ${token}`,
-                },
-                body: JSON.stringify(data),
+                method: "POST", headers: {
+                    "Content-Type": "application/json", "Authorization": `Bearer ${token}`,
+                }, body: JSON.stringify(data),
             });
 
             if (!response.ok) {
                 const errorData = await response.json();
                 Swal.fire({
-                    title: "Oh no, ha sucedido un error!",
-                    text: `${errorData}`,
-                    icon: "error",
+                    title: "Oh no, ha sucedido un error!", text: `${errorData}`, icon: "error",
                 });
             } else {
                 const result = await response.json();
                 Swal.fire({
-                    text: "Registro exitoso, tu grupo ha sido solicitado",
-                    icon: "success",
+                    text: "Registro exitoso, tu grupo ha sido solicitado", icon: "success",
                 });
                 navigate('/students/inicio');
 
             }
         } catch (error) {
             Swal.fire({
-                title: "Oh no, ha sucedido un error!",
-                text: `${error}`,
-                icon: "error",
+                title: "Oh no, ha sucedido un error!", text: `${error}`, icon: "error",
             });
         }
     }
 
 
+    // Trigger when `carrera` changes
+    useEffect(() => {
+        getMateriasByClave();
+    }, [carrera]);
 
-    return (
-        <div className="flex items-center justify-center p-4">
-            <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
-                <h2 className="text-lg font-semibold">Solicitar Grupo</h2>
-                <p className="text-gray-700">Nombre usuario</p>
 
-                <form action={handleSubmit}>
 
-                    <label className="block mt-4">Escoge tu carrera</label>
-                    <select
-                        className="w-full p-2 border rounded"
-                        value={carrera?.label || ""}
 
-                        required
-                        onChange={(e) =>
-                            setCarrera(Constants.carreras.find((c) => c.label === e.target.value))
-                        }
-                    >
-                        <option value="" disabled>-- Selecciona tu carrera --</option>
-                        {Constants.carreras.map((carrera, index) => (
-                            <option key={index} value={carrera.label}>
-                                {carrera.label}
-                            </option>
-                        ))}
-                    </select>
+    return (<div className="flex items-center justify-center p-4">
+        <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
+            <h2 className="text-lg font-semibold">Solicitar Grupo</h2>
+            <p className="text-gray-700">Nombre usuario</p>
 
-                    <label className="block mt-4">Elige una materia</label>
-                    <select
-                        className="w-full p-2 border rounded"
-                        value={matSelec?.nombre_materia || ""}
-                        required
-                        onChange={(e) =>
-                            setMatSelec(materiasClave.find((m) => m.nombre_materia === e.target.value))
-                        }
-                    >
-                        <option value="" disabled>-- Selecciona una materia --</option>
-                        {materiasClave.map((materia) => (
-                            <option key={materia.clave} value={materia.nombre_materia}>
-                                {materia.nombre_materia}
-                            </option>
-                        ))}
-                    </select>
+            <form action={handleSubmit}>
 
-                    <p className="mt-2 text-sm text-gray-600">Grupo: Sin Asignar</p>
-                    <p className="text-sm text-gray-600">Créditos: {matSelec.creditos}</p>
-                    <p className="text-sm text-gray-600">Aula: Sin Asignar</p>
-                    <p className="text-sm text-gray-600">Hrs/Semana: {matSelec.horas}</p>
-                    <p className="text-sm text-gray-600">Clave: {matSelec.clave}</p>
+                <label className="block mt-4">Escoge tu carrera</label>
+                <select
+                    className="w-full p-2 border rounded"
+                    value={carrera?.label || ""}
 
-                    <label className="block mt-4">Elige un turno:</label>
-                    <select
-                        className="w-full p-2 border rounded"
-                        value={turno}
-                        onChange={(e) => setTurno(e.target.value)}
-                    >
-                        <option value="Matutino">Matutino</option>
-                        <option value="Vespertino">Vespertino</option>
-                    </select>
+                    required
+                    onChange={(e) => {
+                        const selectedCarrera = Constants.carreras.find((c) => c.label === e.target.value);
+                        console.log("Selected carrera:", selectedCarrera);
+                        setCarrera(selectedCarrera);
 
-                    <label className="block mt-4">¿Tu profesor trabaja en la institución?</label>
-                    <div className="flex gap-4">
-                        <label className="flex items-center">
-                            <input
-                                type="radio"
-                                name="profesor"
-                                value="Sí"
-                                className="mr-2"
-                                checked={trabajaLocal === "Sí"}
-                                onChange={() => setTrabajaLocal("Sí")}
-                            />
-                            Sí
-                        </label>
-                        <label className="flex items-center">
-                            <input
-                                type="radio"
-                                name="profesor"
-                                value="No"
-                                className="mr-2"
-                                checked={trabajaLocal === "No"}
-                                onChange={() => setTrabajaLocal("No")}
-                            />
-                            No
-                        </label>
-                    </div>
 
-                    <div>
-                        <label className="block mt-4">Sugerir a un profesor</label>
+                        // Reset all dependent fields
+                        setMatSelec('');
+                        setCreditos('');
+                        setHrsSemana('');
+                        setClaveMateria('');
+
+
+
+
+                    }}
+
+                >
+                    <option value="" disabled>-- Selecciona tu carrera --</option>
+                    {Constants.carreras.map((carrera, index) => (<option key={index} value={carrera.label}>
+                        {carrera.label}
+                    </option>))}
+                </select>
+
+                <label className="block mt-4">Elige una materia</label>
+                <select
+                    className="w-full p-2 border rounded"
+                    value={matSelec?.nombre_materia || ""}
+                    required
+                    onChange={(e) => {
+
+                         var selectedMateria = materiasClave.find((m) => m.nombre_materia === e.target.value);
+                        console.log("Selected Materia:", selectedMateria); // Log the selected materia
+                        setMatSelec(selectedMateria);
+
+
+                        setCreditos(selectedMateria.creditos);
+                        setHrsSemana(selectedMateria.horas_semana);
+                        setClaveMateria(selectedMateria.clave_materia);
+
+
+                    }}
+                >
+                    <option value="" disabled>-- Selecciona una materia --</option>
+                    {materiasClave.map((materia) => (<option key={materia.clave} value={materia.nombre_materia}>
+                        {materia.nombre_materia}
+                    </option>))}
+                </select>
+
+                <p className="mt-2 text-sm text-gray-600">
+                    <strong>Grupo:</strong> Sin Asignar
+                </p>
+                <p className="text-sm text-gray-600">
+                    <strong>Créditos:</strong> {creditos}
+                </p>
+                <p className="text-sm text-gray-600">
+                    <strong>Aula:</strong> Sin Asignar
+                </p>
+                <p className="text-sm text-gray-600">
+                    <strong>Hrs/Semana:</strong> {hrsSemana}
+                </p>
+                <p className="text-sm text-gray-600">
+                    <strong>Clave:</strong> {claveMateria}
+                </p>
+
+                <label className="block mt-4">Elige un turno:</label>
+                <select
+                    className="w-full p-2 border rounded"
+                    value={turno}
+                    onChange={(e) => setTurno(e.target.value)}
+                >
+                    <option value="Matutino">Matutino</option>
+                    <option value="Vespertino">Vespertino</option>
+                </select>
+
+                <label className="block mt-4">¿Tu profesor trabaja en la institución?</label>
+                <div className="flex gap-4">
+                    <label className="flex items-center">
                         <input
-                            type="text"
-                            className="w-full p-2 border rounded"
-                            value={profesor}
-                            onChange={(e) => setProfesor(e.target.value)}
-                            placeholder="Nombre del profesor"
+                            type="radio"
+                            name="profesor"
+                            value="Sí"
+                            className="mr-2"
+                            checked={trabajaLocal === "Sí"}
+                            onChange={() => setTrabajaLocal("Sí")}
                         />
-                    </div>
+                        Sí
+                    </label>
+                    <label className="flex items-center">
+                        <input
+                            type="radio"
+                            name="profesor"
+                            value="No"
+                            className="mr-2"
+                            checked={trabajaLocal === "No"}
+                            onChange={() => setTrabajaLocal("No")}
+                        />
+                        No
+                    </label>
+                </div>
 
-                    {trabajaLocal === "No" && (
-                        <div>
-                            <label className="block mt-4">Agrega el correo del profesor</label>
-                            <input
-                                type="email"
-                                className="w-full p-2 border rounded"
-                                value={correoProfesor}
-                                onChange={(e) => setCorreoProfesor(e.target.value)}
-                                placeholder="Correo electrónico"
-                            />
-                        </div>
-                    )}
+                <div>
+                    <label className="block mt-4">Sugerir a un profesor</label>
+                    <input
+                        type="text"
+                        className="w-full p-2 border rounded"
+                        value={profesor}
+                        onChange={(e) => setProfesor(e.target.value)}
+                        placeholder="Nombre del profesor"
+                    />
+                </div>
 
-                    <button type="submit"
-                            className="w-full bg-indigo-500 text-white p-2 rounded-lg hover:bg-indigo-600 transition mt-4">
-                        Solicitar grupo
-                    </button>
-                </form>
-            </div>
+                {trabajaLocal === "No" && (<div>
+                    <label className="block mt-4">Agrega el correo del profesor</label>
+                    <input
+                        type="email"
+                        className="w-full p-2 border rounded"
+                        value={correoProfesor}
+                        onChange={(e) => setCorreoProfesor(e.target.value)}
+                        placeholder="Correo electrónico"
+                    />
+                </div>)}
+
+                <button type="submit"
+                        className="w-full bg-indigo-500 text-white p-2 rounded-lg hover:bg-indigo-600 transition mt-4">
+                    Solicitar grupo
+                </button>
+            </form>
         </div>
-    );
+    </div>);
 }
--- src/utils/constants/Constants.jsx
+++ src/utils/constants/Constants.jsx
@@ -1,14 +1,14 @@
 class Constants {
     static carreras = [
-        { label: 'Ingeniería Civil', clave: 'ICIV-2010-225' },
+        { label: 'Ingeniería Civil', clave: 'ICIV-2010-208' },
         { label: 'Ingeniería en Sistemas Computacionales', clave: 'ISIC-2010-224' },
-        { label: 'Ingeniería Electromecánica', clave: 'IELE-2010-226' },
+        { label: 'Ingeniería Electromecánica', clave: 'IEME-2010-210' },
         { label: 'Ingeniería Industrial', clave: 'IIND-2010-227' },
-        { label: 'Ingeniería en Administración', clave: 'IADM-2010-228' },
-        { label: 'Ingeniería Bioquímica', clave: 'IBQ-2010-229' },
-        { label: 'Ingeniería Química', clave: 'IQ-2010-230' },
-        { label: 'Ingeniería en Gestión Empresarial', clave: 'IGEM-2010-232' },
-        { label: 'Ingeniería en Turismo', clave: 'ITUR-2010-233' }
+        { label: 'Ingeniería en Administración', clave: 'IADNM-2010-213' },
+        { label: 'Ingeniería Bioquímica', clave: 'IBQA-2010-207' },
+        { label: 'Ingeniería Química', clave: 'IQUI-2010-232' },
+        { label: 'Ingeniería en Gestión Empresarial', clave: 'IGEM-2009-201' },
+        { label: 'Ingeniería en Turismo', clave: 'LTUR-2012-237' }
     ];
 
 
